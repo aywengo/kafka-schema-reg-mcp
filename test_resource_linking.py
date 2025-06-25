a48aeda2ec621150a40c5494b30b29b36650ad9c
Roman Melnyk <aywengo@users.noreply.github.com>
2025-06-25 22:12:52 +0200

    Delete test_resource_linking.py

--- test_resource_linking.py
+++
@@ -1,4 +0,0 @@
-# Remove original test_resource_linking.py from root directory
-This file has been moved to tests/test_resource_linking.py
-
-Please use the test file in the tests directory instead.
